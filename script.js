6de95c933a52d24fa910973d44c16dbd6bcd9b1d
Adolfo Vivancos <avivancos@estudiante.edib.es>
2024-10-21 09:23:33 +0200

    Modificación de la estructura del repositorio

---
+++ script.js
@@ -0,0 +1,23 @@
+// Copy all this code and paste to the console of the game
+
+
+
+document.querySelector("#storeBulk100").click();
+
+setInterval(() => {
+    for (let q = 0; q < 21; q++) {
+
+        for (let i = 0; i < 20; i++) {
+            Game.cookies = Infinity;
+            document.querySelector("#product" + i).click();
+        }
+
+
+
+
+    }
+}, 1000);
+
+setInterval(() => {
+    document.querySelector("upgrade0").click();
+}, 1000);
